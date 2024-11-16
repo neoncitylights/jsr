501b333d28458bed3f2bf1c031c1575bdacf91f0
Samantha Nguyen <contact@samanthanguyen.me>
2024-11-15 21:45:44 -0600

    fmt: use single quotes

--- src/mod_test.ts
+++ src/mod_test.ts
@@ -1,5 +1,5 @@
-import { assertEquals } from "@std/assert"
-import { add } from "./mod.ts"
+import { assertEquals } from '@std/assert'
+import { add } from './mod.ts'
 
 Deno.test(function addTest() {
 	assertEquals(add(2, 3), 5)
